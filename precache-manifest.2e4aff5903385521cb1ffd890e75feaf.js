f296ac53b7867cc49d4935a4b3ffd2a439c67161
Samuel Bello <sambello25@gmail.com>
2020-09-30 12:12:32 -0400

    Updates

---
+++ precache-manifest.2e4aff5903385521cb1ffd890e75feaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0eff57b240d3707ad6f54efb78df82",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "2867e130431b8170d0b6",
+    "url": "/movie-search-app/static/css/main.c211d03b.chunk.css"
+  },
+  {
+    "revision": "4b6ddbd0e6a36f817548",
+    "url": "/movie-search-app/static/js/2.ae5eae25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search-app/static/js/2.ae5eae25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2867e130431b8170d0b6",
+    "url": "/movie-search-app/static/js/main.6e6f865e.chunk.js"
+  },
+  {
+    "revision": "93854698e18f7c10401e",
+    "url": "/movie-search-app/static/js/runtime-main.fb6de319.js"
+  }
+]);
